bef60e497d4b38238080ec4d4491033c7fe12d14
PegaKizer <144713395+PegaKizer@users.noreply.github.com>
2024-04-24 16:59:17 +0000

    Console Log

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+//Task 1 
+//Print the first 10 square numbers (1*1 = 1, 2*2 = 4,)
+console.log("task 1")
+for (let i = 0; i < 10; i++) {
+    let squared = i(i)
+    console.log(squared)
+}
+//Task 2
+//Count down from 5 to 1 then say "Blastoff!"
+console.log("task 2")
+for (let i = 5; i > 0; i--) {
+    console.log( "Blastoff!")
+}
+
+
+
+
+//task 4
+//Print the sum of 1 through 100.
+console.log("task 4")
+let sum = 0;
+for (let )
